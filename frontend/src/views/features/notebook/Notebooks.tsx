a4c618cdb3404da21353de1312d9d662d8947fc2
Johanne <johannenacorda@gmail.com>
2025-05-14 00:07:48 +0800

    initial notebooks

--- frontend/src/views/features/notebook/Notebooks.tsx
+++ frontend/src/views/features/notebook/Notebooks.tsx
@@ -2,7 +2,72 @@ import React from 'react'
 
 const Notebooks = () => {
   return (
-    <div>Notebooks</div>
+    <div>
+      <div className="flex z-0">
+        <div className="flex bg-[#FFD25E] p-3 w-1/6 items-center items-start rounded-2xl shadow-lg text-white">
+          <img 
+            src="../../../../../public/add-notebook.png" 
+            alt="Add Notebook" 
+            className="h-5 w-5 inline-block mr-2"
+          />
+          <p>Add New Notebook</p>
+        </div>
+
+        <div className="mt-5">
+          <input 
+            type="text" 
+            placeholder="Search Notebooks..." 
+            className="w-full p-3 rounded-lg border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-[#FFD25E]"
+          />
+        </div>
+
+      </div>
+      
+        {/* notebooks */}
+      <div className="flex flex-row gap-5 pt-10">
+
+        <div className="flex flex-row gap-5 transform transition-transform duration-300 hover:scale-105">
+          <div className="pt-6">
+            <img 
+              src="../../../../../public/binder.png" 
+              alt="Dashboard Illustration" 
+              className=" absolute pl-2 h-42 w-auto z-0"
+            />
+          </div>
+          <div className=" flex flex-col justify-center
+            bg-[#FFD25E] 
+            h-55 w-40 rounded-2xl z-10 
+            shadow-lg pl-2
+            ">
+            <h3 className="text-xl font-bold text-white ">Notebook 3</h3>
+            <p className="text-white text-sm">Description of notebook 3</p>
+          </div>
+        </div>
+        
+        <div className="flex flex-row gap-5 transform transition-transform duration-300 hover:scale-105">
+          <div className="pt-6">
+            <img 
+              src="../../../../../public/binder.png" 
+              alt="Dashboard Illustration" 
+              className=" absolute pl-2 h-42 w-auto z-0"
+            />
+          </div>
+            <div 
+            className="flex flex-col justify-center
+            bg-[#EF988F] 
+            h-55 w-40 rounded-2xl z-10 
+            shadow-lg pl-2 
+            ">
+            <h3 className="text-xl font-bold text-white ">Notebook 3</h3>
+            <p className="text-white text-sm">Description of notebook 3</p>
+            </div>
+        </div>
+
+
+      
+      </div>
+
+    </div>
   )
 }
 
