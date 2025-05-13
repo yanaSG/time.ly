e19a5386b5a2e70b6e96568cb1249327eb593f9b
Johanne <johannenacorda@gmail.com>
2025-05-14 02:02:26 +0800

    improved dashboard

--- frontend/src/views/features/dashboard/Dashboard.tsx
+++ frontend/src/views/features/dashboard/Dashboard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard: React.FC = () => {
   return (
-    <div className="flex flex-col relative"> 
+    <div className="fixed flex flex-col relative pl-10 "> 
 
       <div className="flex justify-center z-0">
         <img 
@@ -13,115 +13,59 @@ const Dashboard: React.FC = () => {
       </div>
         
 
-      <div className='flex flex-row gap-5 p-5 z-10'>
+      <div className='fixed flex flex-row gap-5 p-5 z-10 justify-center pl-60 '>
 
-        <div className="bg-[rgba(255,255,255,0.85)] p-5 rounded-2xl z-10 shadow-lg">
-            <h3 className="text-4xl font-bold text-[#037682] ">Good day, Johanne! </h3>
-          <p className="text-[#037682] ">Ready for another study session?</p>
-        </div>
+        {/* start of id */}
+        <div className="bg-white h-75 w-120 rounded-3xl border border-grey flex flex-col shadow-lg align-center justify-center items-center transform transition-transform duration-300 hover:scale-105">
 
-        <div className="bg-[rgba(255,255,255,0.85)] p-5 rounded-2xl flex shadow-lg">
-            <div className="flex flex-col align-center justify-center">
-              <div className="flex flex-row gap-3 align-center justify-center">
-                <div className="w-10 h-10 bg-[#8BCBC0] rounded-full flex items-center justify-center">
-                  <span>M</span>
-                </div>
-                <div className="w-10 h-10 bg-[#8BCBC0] rounded-full flex items-center justify-center">
-                  <span>T</span>
-                </div>
-                <div className="w-10 h-10 bg-[#8BCBC0] rounded-full flex items-center justify-center">
-                  <span>W</span>
-                </div>
-                <div className="w-10 h-10 bg-[#8BCBC0] rounded-full flex items-center justify-center">
-                  <span>Th</span>
-                </div>
-                <div className="w-10 h-10 bg-[#8BCBC0] rounded-full flex items-center justify-center">
-                  <span>F</span>
-                </div>
-                <div className="w-10 h-10 bg-[#8BCBC0] rounded-full flex items-center justify-center">
-                  <span>Sa</span>
-                </div>
-                <div className="w-10 h-10 bg-[#8BCBC0] rounded-full flex items-center justify-center">
-                  <span>S</span>
-                </div>
-              </div>
-            </div>
-        </div>
-
-       <div className="bg-[rgba(255,255,255,0.85)] rounded-lg">
-          <p>block 1 </p>
-        </div>
-
-      </div>
-
-      {/*  start notebooks */}
-      <div className="flex flex-col pt-10 items-start z-10">
-        {/*  top notebooks */}
-        <div className="flex flex-row">
-          <div>
-            <p className="text-4xl font-bold pr-200">Notebooks</p>
+          <div className=" border border-black bg-[#FFD25E]  pt-2 pb-2 pl-10 pr-10 rounded-full">
+  
           </div>
-          
-          <div className="">
-            <input
-              type="text"
-              placeholder="Search"
-              className="w-full p-3 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+
+          <div className="flex flex-row justify-center items-center items-start pt-10">
+            <img 
+              src="../../../../../public/user-jennie.jpg" 
+              alt="user" 
+              className=" h-45 w-45 rounded-lg shadow-lg"
             />
-          </div>
-        </div>
-         {/*  bottom notebooks */}
-        <div>
-          <div className="flex flex-row gap-5">
-
-            <div className="flex flex-row gap-5">
-              <div className="pt-6">
-                <img 
-                  src="../../../../../public/binder.png" 
-                  alt="Dashboard Illustration" 
-                  className=" absolute h-75 w-auto z-0"
-                />
-              </div>
-              <div className="bg-[#FFD25E] pt-35 pb-35 pl-5 pr-5 rounded-3xl z-10 shadow-lg">
-                <h3 className="text-4xl font-bold text-white ">Notebook 3</h3>
-                <p className="text-white ">Description of notebook 3</p>
-              </div>
-            </div>
-            
-            <div className="flex flex-row gap-5">
-              <div className="pt-6">
-                <img 
-                  src="../../../../../public/binder.png" 
-                  alt="Dashboard Illustration" 
-                  className=" absolute h-75 w-auto z-0"
-                />
-              </div>
-              <div className="bg-[#EF988F] pt-35 pb-35 pl-5 pr-5 rounded-3xl z-10 shadow-lg">
-                <h3 className="text-4xl font-bold text-white ">Notebook 3</h3>
-                <p className="text-white ">Description of notebook 3</p>
-              </div>
-            </div>
-
-            <div className="flex flex-row gap-5">
-              <div className="pt-6">
-                <img 
-                  src="../../../../../public/binder.png" 
-                  alt="Dashboard Illustration" 
-                  className=" absolute h-75 w-auto z-0"
-                />
-              </div>
-              <div className="bg-[#037682] pt-35 pb-35 pl-5 pr-5 rounded-3xl z-10 shadow-lg">
-                <h3 className="text-4xl font-bold text-white ">Notebook 3</h3>
-                <p className="text-white ">Description of notebook 3</p>
-              </div>
-            </div>
           
+            <div className="flex flex-col justify-center items-center align-center mr-30">
+             <p>Name: Jennie</p>
+             <p>Course: Nursing</p>
+             <p>Likes: k-pop</p>
+            </div>
+
           </div>
 
+          <div>
+            <p className="text-xs pt-3">A premium time.ly member for five years.</p>
+          </div>
+          
         </div>
+         {/* end of id */}
+
+        <div className="border border-black bg-white h-75 w-50 rounded-3xl shadow-lg transform transition-transform duration-300 hover:scale-105 p-5 flex flex-col items-center">
+          <p className="font-bold text-lg mb-3">Schedule</p>
+            <ul className="text-sm ">
+            <li className="bg-white rounded-full ">8:00 AM - GE PE</li>
+            <li className="border border-gray-300">10:00 AM - GE UTS</li>
+            <li className="border border-gray-300">12:00 PM - Lunch Break</li>
+            <li className="border border-gray-300">2:00 PM - History Lecture</li>
+            <li className="border border-gray-300">4:00 PM - Gym Session</li>
+            </ul>
+        </div>
+
+         <div className="bg-white h-75 w-50 rounded-3xl shadow-lg transform transition-transform duration-300 hover:scale-105">
+          <p>streak</p>
+        </div>
+
 
       </div>
-      {/*  end notebooks */}
+
+      <div className="bg-white h-75 mt-90 rounded-lg">
+        <p className="p-10">bulletin board</p>
+      </div>
+      
     </div>
   )
 }
